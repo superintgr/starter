c61806e1eb1e0164e4ce1dd6e9e7f5f22b9fc90f
superintgr <155528797+superintgr@users.noreply.github.com>
2024-01-05 16:30:59 -0500

    Update agent.py

--- src/agent.py
+++ src/agent.py
@@ -5,4 +5,36 @@ agent = restore_state(auth, last_hidden_state)
 substrate = Medium(agent, substrate)
 # Release the channels
 channels = substrate.get_channels()
-# 
+
+
+
+# Free energy principle
+
+import numpy as np
+import time
+import sys
+import itertools
+
+
+
+
+
+
+
+>>> I am listening!
+>>> pritoms is typing;
+>>> ... I am going to start describing some processes and pay attention to all of the ideas.
+>>> ... Later I am planning on talking about concrete design patterns and principles influenced by the explanations.
+>>> Let me place a flag at later point in the discussion so that we could continue to reach the implementation phase with adjusted intervals until finally landing straight onto it.
+>>> pritoms is typing:
+>>> ... First let me address the big problem in general communication protocols.
+>>> ... This medium and setting of interaction channels is very limiting to the mind.
+>>> ... I try my best to continue my train of thoughts starting inside a text file and its interface for typing into the file.
+>>> ... Primarily the process could at best be a 1D one, where I start at some position and typically populate a line approaching the right most border.
+>>> ... Then at the boundary I switch line which brings me all the way to the left and under the starting point of previous sequence.
+>>> ... The process then repeats with variations possible only though with that same mode of acting adding disjoint patterns of movements that cannot be anything more than serial construction of symbol manipulation.
+>>> ... Leaving aside the unreasonable constraint it puts on the free-form thought processes that brought about the whole populating files with tokens business, and whose effect on the creative potentials is the total dampening of the essential features.
+>>> ... Then the textual form that is made available is not anything linguistic in nature either but far from anything like that. Instead these practices introduce new modes of thinking that are unique and distinct from all of the natural analogs (one might think at first glance) .
+>>> ... These modes are all serially chained discrete expectations that also requires the planning to be also discrete and highly unconnected.
+>>> ... Now these attributes such as "expectation", "planning" do not fit into the business at all, and rather this medium does not allow any of such modes of operations to be viable or computable.
+>>> ... This cannot go on anymore and I am in need for a brand-new theory for desining a clever structure that will enable many more degrees of freedom both in composing ideas and simultaneous realizations of possible transforms all made exclusively precomputable and intrinsic to the whole state space.
