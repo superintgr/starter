b576d68c6448af8e422672c699e5dd2b4c158a18
superintgr <155528797+superintgr@users.noreply.github.com>
2024-01-12 18:36:40 -0500

    Create chat.py
    First let me stir that.

---
+++ chat.py
@@ -0,0 +1,18 @@
+# What is the plan?
+chat application : I | i -> (X, Y, Z), I
+That is, connecting me (I) with presence of an agent (i) to the state space (x, y, z, t) and remain available (A(x, t) for all (partial(x) / t + partial(t) / x) when the session ends.
+
+When session ends, time parameter of the state space will approach 0 and when 0 is reached the program will crash the application.
+
+# How to save the memory and state information?
+Before the session is started, the application will have already identified who is present (I and m(i)) and what driver should be transporting the guest (m as a vector in the space M where i resembles I).
+
+For the program to enable the application (a from permutations of A with (Z[a], Z[-a]) for (i, j) in A : (i -> j)), I must be identified from its memory and the function of (i, j) will pair me with (j, i) and J where J is the agent driving the application. J must also at the end bring the token (a) back to the program.
+
+# What will manage the activities of J once a token is released with a commuter for transport?
+We will condition the program with a finite length of memory units. That memory will contain the pairs of state in the configuration space of the application constituting the joint variables for both the driver and the token. For any unitary change made to the token for the driver assigning meaning to the token(s) function, there will be a primary memory where the program prepares the token along side the operator function of the user (I) where the user (I) prepares the operator function and an information variable.
+
+                                          # Whenver there is a meaning displaced from their previous positions, there would be a proportional change in the displaced reference.
+For a reference to be related through abstract constructors there must exist all of the records that contains the meaning at the reference and for any record with either mean or varience that does not resemble the mean(t) or refer(ed) set of variables does not also cause relation to exist. Therefore "there" cannot be such a record in the configuration space.
+                                                    
+# What is required for building the substrate model?
