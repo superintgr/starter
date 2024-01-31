675c709e7b12c5d3c995001677a9f5d8fb3d2710
superintgr <155528797+superintgr@users.noreply.github.com>
2024-01-31 02:57:28 -0500

    Create static.py

---
+++ src/stars/static.py
@@ -0,0 +1,17 @@
+"""[basic format]
+
+[inverted high] image ..
+[inverted low] description ..
+[controlled low] object ..
+[controlled high] method ..
+"""
+
+
+block = {
+    "-1" : "+1",
+    "-0" : "+0",
+    "+0" : "-0",
+    "+1" : "-1"
+}
+
+[see static @ static.py]
